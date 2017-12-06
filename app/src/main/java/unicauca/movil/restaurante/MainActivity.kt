89a74bb796c3553b26ae31313888554c9604316d
Dario Chamorro <dario.chamorro.v@gmail.com>
2017-12-05 20:44:40 -0500

    Restaurant layout implemented

---
+++ app/src/main/java/unicauca/movil/restaurante/MainActivity.kt
@@ -0,0 +1,12 @@
+package unicauca.movil.restaurante
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
